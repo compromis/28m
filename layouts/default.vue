1ab8a3f732665344dfbe857ff53d424369f2a067
guillem <guillem@disedit.com>
2023-04-04 17:39:27 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+</script>
+
+<template>
+  <div>
+    <div>
+      Language switcher:
+      <NuxtLink v-for="locale in locales" :key="locale.code" :to="switchLocalePath(locale.code)">
+        {{ locale.name }}
+      </NuxtLink>
+    </div>
+
+    Nuxt: <NuxtPage />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    '@compromis/blobby/scss/blobby.scss',
+    '~/assets/scss/global.scss'
+  ],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'https://compromis.net/sub/icons/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://compromis.net/sub/icons/favicon32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: 'https://compromis.net/sub/icons/favicon192.png' },
+        { rel: 'stylesheet', href: 'https://compromis.net/sub/fonts/manrope.css?variable' }
+      ]
+    }
+  },
+
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+
+  i18n: {
+    locales: [
+      { code: 'val', file: 'val.js', name: 'Valencià' },
+      { code: 'cas', file: 'cas.js', name: 'Castellano' }
+    ],
+    lazy: true,
+    langDir: 'lang',
+    defaultLocale: 'val'
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@compromis/blobby/scss/variables";
+            @import "bootstrap/scss/functions";
+            @import "bootstrap/scss/variables";
+            @import "bootstrap/scss/maps";
+            @import "bootstrap/scss/mixins";
+          `
+        }
+      }
+    }
+  }
+})
