4946527afe75160cf80e3baef26a2b398844af27
guillem <guillem@disedit.com>
2023-05-08 20:07:41 +0200

    Update favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,10 +22,9 @@ export default defineNuxtConfig({
         { name: 'color-scheme', content: 'light only' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'https://compromis.net/sub/icons/favicon.ico' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://compromis.net/sub/icons/favicon32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '192x192', href: 'https://compromis.net/sub/icons/favicon192.png' },
-        { rel: 'stylesheet', href: 'https://compromis.net/sub/fonts/manrope.css?variable' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: 'images/favicon192.png' },
       ]
     }
   },
